e49869d1e5b959db0daa7168349be5cc93346c53
dataplayer12 <jaiyamsharma@gmail.com>
2023-07-15 15:16:44 +0900

    make app accessible from outside

--- embed.py
+++ embed.py
@@ -126,4 +126,4 @@ def update_link_and_abstract(clickData):
 
 # Run the application
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, host='0.0.0.0', port=80)
