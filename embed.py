5b37d57baa783122ffb5cf10972c65bbbf7276d5
dataplayer12 <jaiyamsharma@gmail.com>
2023-07-16 01:19:50 +0900

    allow using openai or spacy embeddings

--- embed.py
+++ embed.py
@@ -7,10 +7,9 @@ import pandas as pd
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output, State
+import os
 
-
-with open('testfile.txt', 'w') as f:
-    f.write('hello world')
+EMBED_TYPE='openai' # or 'spacy'
 
 # Load the spaCy model
 nlp = spacy.load('en_core_web_md')
@@ -46,11 +45,23 @@ for abstract in abstracts:
     if not field_found:
         paper_fields.append('other')
 
-# Embed the abstracts in a vector space
-embeddings = np.array([nlp(abstract).vector for abstract in abstracts])
+
+file_to_seek = 'cvpr-2023-embeddings.npy' if EMBED_TYPE=='spacy' else 'cvpr_2023_embeddings_openai.npy'
+
+if os.path.exists(file_to_seek):
+    embeddings = np.load(file_to_seek)
+    print('Read cached embeddings array')
+else:
+    if EMBED_TYPE=='spacy':
+        # Embed the abstracts in a vector space
+        embeddings = np.array([nlp(abstract).vector for abstract in abstracts])
+        np.save(file_to_seek, embeddings)
+        print('Saved embeddings array')
+    else:
+        raise OSError('User wants to use embeddings from OpenAI API but none were found')
 
 # Reduce the dimensionality of the embeddings to 2D using t-SNE
-tsne = TSNE(n_components=2, random_state=0, perplexity=10)
+tsne = TSNE(n_components=2, random_state=0, perplexity=30)
 embeddings_2d = tsne.fit_transform(embeddings)
 
 # Create a DataFrame with the 2D embeddings, the titles, the fields, and the PDF links
@@ -77,7 +88,7 @@ for field in df['field'].unique():
             size=8
         ),
         text=df_field['title'],
-        customdata=df_field[['pdf_link', 'abstract']].values,
+        customdata=df_field[['pdf_link', 'abstract', 'title']].values,
         hovertemplate=
         '<b>%{text}</b><br><br>' +
         '<extra></extra>'
@@ -85,9 +96,8 @@ for field in df['field'].unique():
 
 # Set the aspect ratio of the figure to 1 and the background color to white
 fig.update_layout(
-    autosize=False,
-    width=800,
-    height=800,
+    autosize=True,
+    height=800,  # fallback height in pixels
     margin=dict(
         l=50,
         r=50,
@@ -95,9 +105,18 @@ fig.update_layout(
         t=100,
         pad=4
     ),
-    paper_bgcolor="white",
-    plot_bgcolor="white"
+    paper_bgcolor="#111111",
+    plot_bgcolor="#111111",
+    font=dict(
+        color="white"
+    ),
+    title="t-SNE plot of CVPR 2023 Paper Embeddings",
+    title_font=dict(
+        size=24
+    ),
+    title_x=0.5
 )
+
 
 # Create a Dash application
 app = dash.Dash(__name__)
@@ -106,23 +125,32 @@ app = dash.Dash(__name__)
 app.layout = html.Div([
     dcc.Graph(
         id='scatter-plot',
-        figure=fig
+        figure=fig,
+        style={"height": "80vh"}  # 80% of the viewport height
     ),
     html.Div(id='pdf-link'),
-    html.Div(id='abstract')
-])
+    html.Div(id='title'),
+    html.Div(id='abstract'),
+    html.Div([
+        dcc.Link('GitHub', href='https://github.com/dataplayer12/cvpr-explorer', target='_blank', style={'display': 'inline-block', 'backgroundColor': '#6c757d', 'color': 'white', 'padding': '10px', 'textDecoration': 'none', 'marginRight': '10px'}),
+        dcc.Link('LinkedIn', href='https://linkedin.com/in/jaiyam-sharma', target='_blank', style={'display': 'inline-block', 'backgroundColor': '#6c757d', 'color': 'white', 'padding': '10px', 'textDecoration': 'none'})
+    ], style={'marginTop': '20px'})
+], style={'backgroundColor': '#111111', 'color': 'white', 'display': 'flex', 'flexDirection': 'column'})
+
 
 # Define a callback that updates the PDF link and the abstract when a point is clicked
 @app.callback(
-    [Output('pdf-link', 'children'), Output('abstract', 'children')],
+    [Output('pdf-link', 'children'), Output('title', 'children'), Output('abstract', 'children')],
     Input('scatter-plot', 'clickData')
 )
 def update_link_and_abstract(clickData):
     if clickData is not None:
-        href, abstract = clickData['points'][0]['customdata']
-        return html.A('Open PDF', href=href, target='_blank'), html.P("Abstract: " + abstract)
+        href, abstract, title = clickData['points'][0]['customdata']
+        return html.A('Open PDF', href=href, target='_blank', style={'color': 'white'}), html.P("Title: " + title, style={'color': 'white'}), html.P("Abstract: " + abstract, style={'color': 'white'})
     else:
-        return '', ''
+        return '', '', ''
+
+
 
 # Run the application
 if __name__ == '__main__':
