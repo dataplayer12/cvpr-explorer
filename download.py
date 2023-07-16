36a44b1e5a42d4c69e46c721195dc26a0a7edc27
dataplayer12 <jaiyamsharma@gmail.com>
2023-07-16 13:24:42 +0900

    download all cvpr papers

--- download.py
+++ download.py
@@ -6,11 +6,24 @@ from bs4 import BeautifulSoup
 import json
 import time
 
-urls = [
-    "https://openaccess.thecvf.com/CVPR2023?day=2023-06-20",
-    "https://openaccess.thecvf.com/CVPR2023?day=2023-06-21",
-    "https://openaccess.thecvf.com/CVPR2023?day=2023-06-22"
-    ]
+BASE_URL="https://openaccess.thecvf.com/"
+
+yeartopage = {
+    2023 : ['CVPR2023?day=2023-06-20', 'CVPR2023?day=2023-06-21', 'CVPR2023?day=2023-06-22'],
+    2022 : ['CVPR2022?day=2022-06-21', 'CVPR2022?day=2022-06-22', 'CVPR2022?day=2022-06-23', 'CVPR2022?day=2022-06-24'],
+    2021 : ['CVPR2021?day=2021-06-21', 'CVPR2021?day=2021-06-22', 'CVPR2021?day=2021-06-23', 'CVPR2021?day=2021-06-24', 'CVPR2021?day=2021-06-25'],
+    2020 : ['CVPR2020?day=2020-06-16', 'CVPR2020?day=2020-06-17', 'CVPR2020?day=2020-06-18'],
+    2019 : ['CVPR2019?day=2019-06-18', 'CVPR2019?day=2019-06-19', 'CVPR2019?day=2019-06-20'],
+    2018 : ['CVPR2018?day=2018-06-19', 'CVPR2018?day=2018-06-20', 'CVPR2018?day=2018-06-21'],
+    2017 : ['CVPR2017'],
+    2016 : ['CVPR2016'],
+    2015 : ['CVPR2015'],
+    2014 : ['CVPR2014'],
+    2013 : ['CVPR2013']
+}
+
+
+yeartourl = {year: [BASE_URL+ day for day in yeartopage[year]] for year in yeartopage.keys()}
 
 # Setup Chrome options
 chrome_options = webdriver.ChromeOptions()
@@ -22,43 +35,51 @@ chrome_options.add_argument("--remote-debugging-port=9222")  # This line should 
 # Choose Chrome Browser
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
-data = []
 
-for url in urls:
-    driver.get(url)
+for year in range(2013, 2024):
+    data = []
+    urls=yeartourl[year]
+    print()
+    print('==============================')
+    print(f'Processing papers for CVPR {year}')
+    print('==============================')
+    print()
+    
+    for url in urls:
+        driver.get(url)
 
-    # Find all paper links
-    paper_links = driver.find_elements(By.CSS_SELECTOR, '.ptitle a')
+        # Find all paper links
+        paper_links = driver.find_elements(By.CSS_SELECTOR, '.ptitle a')
 
-    # Collect all the URLs of the papers
-    paper_urls = [paper.get_attribute('href') for paper in paper_links]
+        # Collect all the URLs of the papers
+        paper_urls = [paper.get_attribute('href') for paper in paper_links]
 
-    # paper_urls = paper_urls[:10]
+        # paper_urls = paper_urls[:10]
 
-    for idx, paper_url in enumerate(paper_urls):
-        driver.get(paper_url)
+        for idx, paper_url in enumerate(paper_urls):
+            driver.get(paper_url)
 
-        # Wait for the new page to load
-        time.sleep(2)
+            # Wait for the new page to load
+            time.sleep(2)
 
-        # Parse the new page with BeautifulSoup
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
+            # Parse the new page with BeautifulSoup
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-        # Find the title, abstract, and PDF link
-        title = soup.find('div', id='papertitle').text.strip()
-        print(f"Processing paper {idx+1} of {len(paper_urls)}: {title}")
-        abstract = soup.find('div', id='abstract').text.strip()
-        pdf_link = 'https://openaccess.thecvf.com/' + soup.find('a', text='pdf')['href']
-        
-        data.append({
-            'title': title,
-            'abstract': abstract,
-            'pdf_link': pdf_link
-        })
+            # Find the title, abstract, and PDF link
+            title = soup.find('div', id='papertitle').text.strip()
+            print(f"Processing paper {idx+1} of {len(paper_urls)}: {title}")
+            abstract = soup.find('div', id='abstract').text.strip()
+            pdf_link = 'https://openaccess.thecvf.com/' + soup.find('a', text='pdf')['href']
+            
+            data.append({
+                'title': title,
+                'abstract': abstract,
+                'pdf_link': pdf_link
+            })
 
 
-with open('cvpr_2023_papers.json', 'w') as f:
-    json.dump(data, f)
+    with open(f'data/cvpr_{year}_papers.json', 'w') as f:
+        json.dump(data, f)
 
 
 driver.quit()
